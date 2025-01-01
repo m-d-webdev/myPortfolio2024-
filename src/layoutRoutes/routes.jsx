6c18278da879dec9ac1ba55fae730288c9526a0b
m-d-webdev <mstph.iderkaoui@gmail.com>
2025-01-01 01:02:43 +0100

    add projects

--- src/layoutRoutes/routes.jsx
+++ src/layoutRoutes/routes.jsx
@@ -2,24 +2,44 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "../pages/home";
 import NotFound from "./notFound";
-import Aboute_me from "../pages/aboute_me";
+import Skills from '../pages/Skills'
 const routes = createBrowserRouter([
     {
         element: <Layout />,
         children: [
             {
                 path: "/",
-                element: <Home />
+                element: <Home page_open={0} />
 
             },
             {
                 path: "/home",
-                element: <Home />
+                element: <Home page_open={0} />
+
+            },
+            {
+                path: "/projects",
+                element: <Home page_open={2}  />
 
             },
             {
                 path: "/me",
-                element: <Aboute_me />
+                element: <Home page_open={3} />
+
+            },
+            {
+                path: "/skills",
+                element: <Home page_open={1}  />
+
+            },
+            {
+                path: "/skills2",
+                element: <Skills  />
+
+            },
+            {
+                path: "/contact",
+                element: <Home page_open={4}  />
 
             },
             {
